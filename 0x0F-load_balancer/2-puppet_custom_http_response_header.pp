ab9be3731df01a236105181843633014172180d3
root <maxfred304@gmail.com>
2023-04-03 23:26:26 -0700

    automate the task of creating a custom HTTP header response, but with Puppet.

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# 2-puppet_custom_http_response_header.pp
+
+# Install the nginx package
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Create a custom HTTP header response
+file { '/etc/nginx/conf.d/custom-header.conf':
+  content => "add_header X-Served-By ${::hostname};",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Restart the nginx service to apply the new configuration
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
